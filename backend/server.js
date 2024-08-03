6bab9ac3784ad540f8d8d44c684eea2d709e9080
mahan-minamoto <monami.somsaha@gmail.com>
2024-08-04 04:24:00 +0530

    feat: login implemented

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const authRoutes = require("./api/routes/auth");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+const { DATABASE_URL } = process.env;
+
+mongoose.connect = mongoose
+  .connect(DATABASE_URL, {
+    ssl: true
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 const app = express();
 
